14599011fda83020be62ed3f277eb29c583345ce
Treadgold <laptoprecording@gmail.com>
2023-12-05 17:47:12 +1300

    day2/problem2.py

--- day2/problem2.py
+++ day2/problem2.py
@@ -1,54 +1,51 @@
 with open('input.txt', 'r') as f:
     lines = f.readlines()
 input = {}
-
 for line in lines:
     game, data = line.split(':')
     #print(game)
     int_game = int(game[5:]) 
     input[int_game] = data
     
-
+    
 
 games = {}
 for key, value in input.items():
     drawn_cubes = value.split(';')
     #print("Game number = ", key)
-    draws = {}
+    draws = []
     for item in drawn_cubes:
+        sample = {}
         for x in item.split(','):
             x = x.strip()
             count, color = x.split(' ')
-            #print(item)
-            #print(count, color)
-            if not color in draws:
-                draws[color] = int(count)
-            else:
-                if draws[color] < int(count):
-                    draws[color] = int(count)
-            #print('')
+            sample[color] = int(count)
+        draws.append(sample)
     games[key] = draws
-colors = {'red': 12, 'green': 13, 'blue': 14}
-results = {}
+games
 
+maxes = {}
 for key, value in games.items():
-    #print("game number = ", key)
-    #print(value)
-    success = True
-    for color in colors:
-        if color in value:
-            if value[color] <= colors[color]:
-                pass
-            else:
-                success = False
-            # if value[color] > colors[color]:
-            #     results[key] = f"{colors[color]} was the maximum available {color}, but {value[color]} {color} blocks were drawn from the bag."
-    results[key] = success    
+    #print(key, value)
+    mr, mg, mb = 0, 0, 0
+    for draw in value:        
+        for color in draw:
+            match color:
+                case 'red':
+                    mr = max(mr, draw[color])
+                case 'green':
+                    mg = max(mg, draw[color])
+                case 'blue':
+                    mb = max(mb, draw[color])
+    maxes[key] = (mr, mg, mb)
     
-final = 0
-for idx in results.keys():
-    if results[idx]:
-        final += idx
+                
+                    
+                
+sums = 0
+for key, value in maxes.items():
+    x, y, z = value[0], value[1], value[2]
+    cb = x*y*z
+    sums += cb
     
-print(final)
-    
+print(sums)
