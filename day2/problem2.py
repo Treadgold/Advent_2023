3760aecf48aa675c3d4d97a8db980ead34892e29
Treadgold <laptoprecording@gmail.com>
2023-12-05 17:57:55 +1300

    better solution

--- day2/problem2.py
+++ day2/problem2.py
@@ -1,47 +1,37 @@
 with open('input.txt', 'r') as f:
     lines = f.readlines()
+
 input = {}
 for line in lines:
     game, data = line.split(':')
     #print(game)
     int_game = int(game[5:]) 
     input[int_game] = data
-    
-    
 
 games = {}
+maxes = {}
 for key, value in input.items():
     drawn_cubes = value.split(';')
     #print("Game number = ", key)
     draws = []
+    mr, mg, mb = 0, 0, 0
     for item in drawn_cubes:
         sample = {}
         for x in item.split(','):
             x = x.strip()
             count, color = x.split(' ')
+            match (color):
+                case 'red':
+                    mr = max(mr, int(count))
+                case 'green':
+                    mg = max(mg, int(count))
+                case 'blue':
+                    mb = max(mb, int(count))
             sample[color] = int(count)
         draws.append(sample)
     games[key] = draws
-games
-
-maxes = {}
-for key, value in games.items():
-    #print(key, value)
-    mr, mg, mb = 0, 0, 0
-    for draw in value:        
-        for color in draw:
-            match color:
-                case 'red':
-                    mr = max(mr, draw[color])
-                case 'green':
-                    mg = max(mg, draw[color])
-                case 'blue':
-                    mb = max(mb, draw[color])
     maxes[key] = (mr, mg, mb)
-    
-                
-                    
-                
+
 sums = 0
 for key, value in maxes.items():
     x, y, z = value[0], value[1], value[2]
